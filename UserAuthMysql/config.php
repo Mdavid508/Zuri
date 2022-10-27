57362b78c8b2936b21b34163bf258dbd5076b93f
Mugo David <mugombataru@outlook.com>
2022-10-27 08:25:50 +0300

    Journey to OOP

--- UserAuthMysql/config.php
+++ UserAuthMysql/config.php
@@ -1,16 +1,19 @@
 <?php
 
+$host = "127.0.0.1";
+$user = "root";
+$db = "zuriphp";
+$password = "";
 
-function db() {
+function db($host, $user, $db, $password)
+{
     //set your configs here
-    $host = "127.0.0.1";
-    $user = "root";
-    $db = "zuriphp";
-    $password = "";
+
     $conn = mysqli_connect($host, $user, $password, $db);
-    if(!$conn){
+    if (!$conn) {
         echo "<script> alert('Error connecting to the database') </script>";
+    } else {
+        // echo "Connected to the database";
     }
     return $conn;
-
 }
--- UserAuthMysql/php/userauth.php
+++ UserAuthMysql/php/userauth.php
@@ -1,6 +1,7 @@
 <?php
-
+// include_once '../config.php';
 require_once "../config.php";
+
 
 
 //register users 
@@ -8,22 +9,28 @@ require_once "../config.php";
 function registerUser($fullnames, $email, $password, $gender, $country)
 {
     //create a connection variable using the db function in config.php
-    $conn = db();
+    $conn = db('127.0.0.1', 'root', 'zuriphp', '');
     //check if user with this email already exist in the database
-    $sql = "SELECT * FROM Students WHERE email = '$email'";
+    $sql = "SELECT * FROM students WHERE email = '$email'";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         echo "<h1 style='color: red'>User with this email already exist</h1>";
     } else {
-        
-    $sql = "INSERT INTO Students (full_names, email, password, country, gender) VALUES ('$fullnames', '$email', '$password', '$country', '$gender')";
-    $result = mysqli_query($conn, $sql);
-    if($result){
-        echo "User registered successfully";
-    }else{
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+
+        $sql = "INSERT INTO students (full_names, email, password, country, gender) VALUES ('$fullnames', '$email', '$password', '$country', '$gender')";
+        $result = mysqli_query($conn, $sql);
+        if (!$result) {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        } else {
+            echo "<h1 style='color: green'>User registered successfully</h1>";
+        }
     }
-    
+    // if($result){
+    //     echo "User registered successfully";
+    // }else{
+    //     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    // }
+
 
 }
 
@@ -33,20 +40,35 @@ function registerUser($fullnames, $email, $password, $gender, $country)
 function loginUser($email, $password)
 {
     //create a connection variable using the db function in config.php
-    $conn = db();
+    $conn = db('127.0.0.1', 'root', 'zuriphp', '');
 
-    echo "<h1 style='color: red'> LOG ME IN (IMPLEMENT ME) </h1>";
+    //creates a user session
+    session_start();
+
+    // echo "<h1 style='color: red'> LOG ME IN (IMPLEMENT ME) </h1>";
     //open connection to the database and check if username exist in the database
     //if it does, check if the password is the same with what is given
     //if true then set user session for the user and redirect to the dasbboard
-    $sql = "SELECT * FROM Students WHERE email = '$email' AND password = '$password'";
+    $sql = "SELECT * FROM students WHERE email = '$email' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        echo "User logged in successfully";
-    } else {
-        // echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        echo "<h1 style='color: red'>Incorrect email or Password</h1>";
+    $row = mysqli_fetch_assoc($result);
+    if(is_array($row)){
+        $_SESSION["email"] = $row['email'];
+        $_SESSION["password"] = $row['password'];
+    }else{
+        echo "<h1 style='color: red'>Invalid username or password</h1>";
+
+    } 
+    if(isset($_SESSION["email"])){
+        header("Location: ../dashboard.php");
     }
+    // if (mysqli_num_rows($result) > 0) {
+    //     echo "<h1 style='color: green'>User logged in successfully</h1>";
+    //     header("location:dashboard.php");
+    // } else {
+    //     echo "<h1 style='color: red'>Incorrect email or Password</h1>";
+        
+    // }
 }
 
 //reset password
@@ -54,26 +76,25 @@ function loginUser($email, $password)
 function resetPassword($email, $password)
 {
     //create a connection variable using the db function in config.php
-    $conn = db();
+    $conn = db('127.0.0.1', 'root', 'zuriphp', '');
     // echo "<h1 style='color: red'>RESET YOUR PASSWORD (IMPLEMENT ME)</h1>";
     //open connection to the database and check if username exist in the database
     //if it does, replace the password with $password given
-    
-        $sql = "UPDATE students SET password = '$password' WHERE email = '$email'";
-        $result = mysqli_query($conn, $sql);
-        if($result){
-            echo "Password reset successful";
-        }else{
-            echo "Password reset failed";
-        }
-    
+
+    $sql = "UPDATE students SET password = '$password' WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "Password reset successful";
+    } else {
+        echo "Password reset failed";
+    }
 }
 //show all users
 //select Query will be used to select data from the database
 function getusers()
 {
-    $conn = db();
-    $sql = "SELECT * FROM Students";
+    $conn = db('127.0.0.1', 'root', 'zuriphp', '');
+    $sql = "SELECT * FROM students";
     $result = mysqli_query($conn, $sql);
     echo "<html>
     <head></head>
@@ -107,15 +128,13 @@ function getusers()
 //delete Query will be used to delete data from the database
 function deleteaccount($id)
 {
-    $conn = db();
+    $conn = db('127.0.0.1', 'root', 'zuriphp', '');
     //delete user with the given id from the database
-    $sql = "DELETE FROM Students WHERE id = '$id'";
+    $sql = "DELETE FROM students WHERE id = '$id'";
     $result = mysqli_query($conn, $sql);
-    if($result){
+    if ($result) {
         echo "User deleted successfully";
-    }else{
+    } else {
         echo "User deletion failed";
     }
-
-}
 }
